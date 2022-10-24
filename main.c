9c3e4c8bcea7bcb351103a7336196ecd2f94e2c6
William Rocha <william.bueno.rocha@gmail.com>
2022-10-24 12:06:21 -0300

    Generating sine PWM output

--- main.c
+++ main.c
@@ -8,7 +8,21 @@
 #include "Peripheral_Setup.h"
 
 uint32_t index = 0;
-uint16_t sine_table[125];
+uint16_t sine_table[125] = { 300, 315, 330, 345, 360, 375, 389, 403, 417, 431,
+                             445, 458, 470, 482, 494, 505, 516, 526, 536, 545,
+                             553, 561, 568, 575, 580, 585, 590, 593, 596, 598,
+                             599, 600, 600, 599, 597, 595, 591, 588, 583, 578,
+                             571, 565, 557, 549, 540, 531, 521, 511, 500, 488,
+                             476, 464, 451, 438, 424, 410, 396, 382, 367, 353,
+                             338, 323, 308, 292, 277, 262, 247, 233, 218, 204,
+                             190, 176, 162, 149, 136, 124, 112, 100, 89, 79, 69,
+                             60, 51, 43, 35, 29, 22, 17, 12, 9, 5, 3, 1, 0, 0,
+                             1, 2, 4, 7, 10, 15, 20, 25, 32, 39, 47, 55, 64, 74,
+                             84, 95, 106, 118, 130, 142, 155, 169, 183, 197,
+                             211, 225, 240, 255, 270, 285 };
+
+uint16_t pwm[100];
+int g = 0;
 
 __interrupt void isr_cpu_timer0(void);
 
@@ -33,14 +47,16 @@ int main(void)
     EDIS;
 
     InitCpuTimers();
-    ConfigCpuTimer(&CpuTimer0, 60, 50000);              // Frequency clock: 60 MHz
+    ConfigCpuTimer(&CpuTimer0, 60, 160);                // Frequency clock: 60 MHz, 160 us interruption
     StartCpuTimer0();                                   // Enable Timer 0 interruption
 
+    // No memory for float compute
+    /*
     for (index = 0; index < 125; index++)
     {
-//        sine_table[index] = (uint16_t) (1000.0 * (1.0 + sin(6.28318531 / 125.0 * ((float) index))));
-        sine_table[index] = 0;
+        sine_table[index] = (uint16_t) (1000.0 * (1.0 + sin(6.28318531 / 125.0 * ((float) index))));
     }
+     */
     index = 0;
 
     EINT;                                               // Enable Global interrupt INTM
@@ -64,6 +80,17 @@ int main(void)
 __interrupt void isr_cpu_timer0(void)
 {
     GpioDataRegs.GPATOGGLE.bit.GPIO0 = 1;
+
+    index = (index == 124) ? 0 : (index + 1);
+
+    EPwm3Regs.CMPA.half.CMPA = sine_table[index];
+    EPwm4Regs.CMPA.half.CMPA = sine_table[index];
+
+    pwm[g] = sine_table[index];
+    if (++g == 100)
+    {
+        g = 0;
+    }
 
     PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
 }
